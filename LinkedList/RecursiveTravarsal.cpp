22f776165a04843b4d987cda89f4ad39bdbdaf8f
Anurag Singh Patel <anuragsinghpatel98@gmail.com>
2021-09-05 23:31:27 +0530

    Recursive Travarsal

---
+++ LinkedList/RecursiveTravarsal.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+class Node{
+	public:
+  		
+     	int data;
+	    Node* next;
+	    Node(int data){
+		    this->data=data;
+		    next=NULL;
+	}
+};
+//Take Input
+Node* TakeInputBetter(){
+	int data;
+	cin>>data;
+	Node* Head=NULL;
+	Node* Tail=NULL;
+	while(data!=-1){
+		Node *newNode=new Node(data);
+		if(Head==NULL){
+			Head=newNode;
+			Tail=newNode;
+		}
+		else{
+			Tail->next=newNode;
+			Tail=Tail->next;
+		}
+		cin>>data;
+	}
+	return Head;
+}
+void  printRecursive(Node* Head){
+    if(Head==NULL)
+            return;
+    cout<<Head->data<<" ";
+    printRecursive(Head->next);
+}
+int main(){
+	Node* Head= TakeInputBetter();
+    printRecursive(Head);
+}
