a6540d1dc7bf5587e3f6fb8af4ebd11c893dc07b
Anurag Singh Patel <anuragsinghpatel98@gmail.com>
2021-09-07 11:08:51 +0530

    Insert At the Begninning Of Doubly LL

---
+++ LinkedList/InserBegninningOfDLL.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+        int data;
+        Node* prev;
+        Node* next;
+        Node(int data){
+        this->data=data;
+        prev=NULL;
+        next=NULL;
+     }
+};
+
+Node* InsertBegninningOfDLL(Node* Head,int x){
+    if(Head==NULL){
+        Node* temp=new Node(x);
+            temp->next=NULL;
+            temp->prev=NULL;
+            return temp;
+    }
+    Node* temp=new Node(x);
+    temp->next=Head;
+    Head->prev=temp;
+    temp->prev=NULL;
+    return temp;
+}
+void PrintDLL(Node* Head){
+    Node* curr=Head;
+    while(curr!=NULL){
+        cout<<curr->data<<" ";
+        curr=curr->next;
+    }
+    cout<<endl;
+}
+int main() 
+{ 
+	Node *head=new Node(10);
+	Node *temp1=new Node(20);
+	Node *temp2=new Node(30);
+	head->next=temp1;
+	temp1->prev=head;
+	temp1->next=temp2;
+	temp2->prev=temp1;
+	head=InsertBegninningOfDLL(head,5);
+	PrintDLL(head);
+	return 0;
+} 
