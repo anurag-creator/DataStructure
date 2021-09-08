fb1fe02921984e6c59af5175b36e27d598c9c0f4
Anurag Singh Patel <anuragsinghpatel98@gmail.com>
2021-09-09 00:13:04 +0530

    Circular Doubly LL

---
+++ LinkedList/CircularDoublyLL.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node{
+    int data;
+    Node *prev;
+    Node* next;
+    Node(int d){
+        data=d;
+        prev=NULL;
+        next=NULL;
+    }
+};
+void printList(Node *Head){
+    if(Head==NULL)return;
+    Node *p=Head;
+    do{
+        cout<<p->data<<" ";
+        p=p->next;
+    }while(p!=Head);
+}
+
+Node *insertAtHead(Node *head,int x){
+    Node *temp=new Node(x);
+    if(head==NULL){
+        temp->next=temp;
+        temp->prev=temp;
+        return temp;
+    }
+    temp->prev=head->prev;
+    temp->next=head;
+    head->prev->next=temp;
+    head->prev=temp;
+    return temp;
+}
+int main() 
+{ 
+	Node *head=new Node(10);
+	Node *temp1=new Node(20);
+	Node *temp2=new Node(30);
+	head->next=temp1;
+	temp1->next=temp2;
+	temp2->next=head;
+	temp2->prev=temp1;
+	temp1->prev=head;
+	head->prev=temp2;
+	head=insertAtHead(head,5);
+	printList(head);
+	return 0;
+} 
