2185f678fd02b496191917c1ac5c43f6e4bf57ac
Anurag Singh Patel <anuragsinghpatel98@gmail.com>
2021-09-13 00:52:09 +0530

    Preorder Travarsal

---
+++ BinaryTree/PreorderTravarsal.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+template<typename T>
+class BinaryTreeNode{
+    public:
+    T data;
+    BinaryTreeNode* left;
+    BinaryTreeNode* right;
+    BinaryTreeNode(T data){
+        this->data=data;
+        left=NULL;
+        right=NULL;
+    }
+    ~BinaryTreeNode(){
+        delete left;
+        delete right;
+    }
+};
+void preorder(BinaryTreeNode<int>* root){
+      if(root!=NULL){
+          cout<<root->data<<" ";
+          preorder(root->left);
+        preorder(root->right);
+      }
+}
+int main(){
+     BinaryTreeNode<int>* root= new BinaryTreeNode<int>(1);
+     BinaryTreeNode<int>* node1= new BinaryTreeNode<int>(2);
+     BinaryTreeNode<int>* node2=new BinaryTreeNode<int>(3);
+    root->left=node1;
+    root->right=node2;
+    preorder(root);
+}
