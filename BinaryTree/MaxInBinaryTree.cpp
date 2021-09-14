ae3b51fb250c7c62d4a51763f7cae711afc4e956
Anurag Singh Patel <anuragsinghpatel98@gmail.com>
2021-09-14 23:54:39 +0530

    Maximum In Binary Tree

---
+++ BinaryTree/MaxInBinaryTree.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node  
+{ 
+  int key; 
+  struct Node *left; 
+  struct Node *right; 
+  Node(int k){
+      key=k;
+      left=right=NULL;
+  }
+};
+
+int maxInBinaryTree(Node* root){
+    if(root==NULL)
+        return INT_MIN;
+    else{
+        return max(root->key,max(maxInBinaryTree(root->left),maxInBinaryTree(root->right)));
+    }
+}
+
+int main() {
+	
+	Node *root=new Node(10);
+	root->left=new Node(20);
+	root->right=new Node(30);
+	root->left->left=new Node(40);
+	root->left->right=new Node(50);
+	root->right->left=new Node(60);
+	root->right->right=new Node(70);
+	
+	cout<<maxInBinaryTree(root);
+}
