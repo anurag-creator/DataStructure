699a0356d5e1792c7e2fb10b4176829f4bb2a90e
Anurag Singh Patel <anuragsinghpatel98@gmail.com>
2021-09-14 23:45:13 +0530

    Size Of BinaryTree

---
+++ BinaryTree/SizeOfBinaryTree.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node  
+{ 
+  int key; 
+  struct Node *left; 
+  struct Node *right; 
+  Node(int k){
+      key=k;
+      left=right=NULL;
+  }
+};
+
+int FindSize(Node* root){
+     if(root==0)return 0;
+    
+         else{
+             return 1+FindSize(root->left)+FindSize(root->right);
+         }
+}
+
+int main() {
+	
+	Node *root=new Node(10);
+	root->left=new Node(20);
+	root->right=new Node(30);
+	root->left->left=new Node(40);
+	root->left->right=new Node(50);
+	root->right->left=new Node(60);
+	root->right->right=new Node(70);
+	
+	cout<<FindSize(root);
+}
