879f9aebc90cbe9913d4bfa14218c48ff1f73fb6
Anurag Singh Patel <anuragsinghpatel98@gmail.com>
2021-10-10 17:17:26 +0530

    Factorial done

---
+++ Recursion/Factorial.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int factorial(int n){
+    if(n==0)return 1;
+    int smallOutput=factorial(n-1);
+    return n* smallOutput;
+}
+int main(){
+    int n;
+    cin>>n;
+    int output=factorial(n);
+    cout<<"Factorial of "<< n <<" is "<<output<<endl;
+
+}
