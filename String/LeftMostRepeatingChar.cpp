4c59d58303983f02ad82488be5e186c862537af4
Anurag Singh Patel <anuragsinghpatel98@gmail.com>
2021-10-16 11:49:19 +0530

    Left MostRepeating char

--- String/LeftMostRepeatingChar.cpp
+++ String/LeftMostRepeatingChar.cpp
@@ -24,6 +24,36 @@ int LeftMostRepeatingChar(string str){
     }
     return -1;
 }
+//Efficient Approach1 
+const int CHAR=256;
+int LeftMostRepeatingChar(string &str){
+    int fIndex[CHAR]={0};
+    fill(fIndex,fIndex+CHAR,-1);
+    int res=INT_MAX;
+    for(int i=0;i<str.length();i++){
+        int fi=fIndex[str[i]];
+        if(fi==-1)
+        fIndex[str[i]]=i;
+        else
+        res=min(res,fi);
+    }
+    return (res==INT_MAX)?-1:res;
+}
+//Efficient Approach2
+const int CHAR=256;
+int LeftMostRepeatingChar(string &str){
+    bool visited[CHAR];
+    fill(visited,visited+CHAR,false);
+    int res=-1;
+    for(int i=str.length()-1;i>=0;i--){
+        if(visited[str[i]])
+        res=i;
+        else
+        visited[str[i]]=true;
+    }
+    
+    return res;
+}
 int main(){
     string str="abbcc";
     cout<<LeftMostRepeatingChar(str);
