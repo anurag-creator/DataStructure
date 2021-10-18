6bdea5935c66c302e33b3f7f0c013c6967d1f814
Anurag Singh Patel <anuragsinghpatel98@gmail.com>
2021-10-18 10:11:34 +0530

    Gp sum done

--- Recursion/GeometricSum.cpp
+++ Recursion/GeometricSum.cpp
@@ -1,11 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
-int GPSum(int x,int n){
+int power(int x,int n){
     if(n!=0)
-        return (x*GPSum(x,n-1));
+        return (x*power(x,n-1));
     else
         return -1;
 }
-int main(){
-
+double geoSum(int k){
+    if(k==0)return 1;
+    int ans=power(2,k);
+    return ((double)1/ans)+geoSum(k-1);
 }
