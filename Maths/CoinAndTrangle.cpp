78e25181f15cdb029ff1910f9e0ccf4083674ba5
Anurag Singh Patel <anuragsinghpatel98@gmail.com>
2021-10-21 00:10:26 +0530

    Coin and Trangle

---
+++ Maths/CoinAndTrangle.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	long long int t;
+	cin>>t;
+	while(t--){
+	       long long int n=0;
+	       cin>>n;
+	       cout<<(long long int)(-1+sqrt(1+8*n))/2<<endl;
+	}
+	return 0;
+}
