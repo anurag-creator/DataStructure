0d43f642b071360af85bf88d5a776060651da500
Anurag Singh Patel <anuragsinghpatel98@gmail.com>
2021-10-21 23:27:08 +0530

    Frequency of Element

---
+++ Hashing/FrequencyOfElement.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+void countFreq(int arr[], int n){
+	unordered_map<int, int> mp;
+	for (int i = 0; i < n; i++)
+		mp[arr[i]]++;
+	for (auto x : mp)
+		cout << x.first << " " << x.second << endl;
+}
+
+int main(){
+	int arr[] = { 10, 20, 20, 10, 10, 20, 5, 20 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	countFreq(arr, n);
+	return 0;
+}
