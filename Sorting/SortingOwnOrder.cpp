0d0b426c55a032911aaf500718372f1606ed7427
Anurag Singh Patel <anuragsinghpatel98@gmail.com>
2021-10-31 11:20:17 +0530

    Sorting Own fn

---
+++ Sorting/SortingOwnOrder.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+struct Point{
+    int x;
+    int y;
+};
+bool myCmp(Point p1,Point p2){
+    return p1.x<p2.x;
+}
+int main(){
+    Point arr[]={{3,10},{2,8},{5,4}};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    sort(arr,arr+n,myCmp);
+    for(auto i:arr)
+           cout<<"("<<i.x<<","<<i.y<<")"<<endl;
+    cout<<endl;
+}
