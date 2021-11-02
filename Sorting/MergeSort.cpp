0cb98dc4bd04b22d060daae5dce9ca2e9549f9ca
Anurag Singh Patel <anuragsinghpatel98@gmail.com>
2021-11-02 22:26:21 +0530

    Merge Sort Done

---
+++ Sorting/MergeSort.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+//Naive Approach
+void merge1(int *a,int *b,int m,int n){
+    int c[m+n];
+    for(int i=0;i<m;i++)
+        c[i]=a[i];
+    for(int j=0;j<n;j++)
+        c[j+m]=b[j];
+    sort(c,c+m+n);
+    for(int i=0;i<m+n;i++)
+        cout<<c[i]<<" ";
+}
+//Efficient
+void merge(int *a,int *b,int m,int n){
+        int i=0,j=0;
+    while(i<m && j<n){
+        if(a[i]<b[j])
+            cout<<a[i++]<<" ";
+        else
+            cout<<b[j++]<<" ";
+    }
+    while(i<m)
+        cout<<a[i++]<<" ";
+    while(j<n)
+        cout<<b[j++]<<" ";  
+}
+int main(){
+    	
+    int a[]={10,15,20,40};
+    int b[]={5,6,6,10,15};
+	int m=sizeof(a)/sizeof(a[0]);
+	int n=sizeof(b)/sizeof(b[0]);
+	merge(a,b,m,n);
+}
