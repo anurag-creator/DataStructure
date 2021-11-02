27e58a25cff65f5c0e569d4a4e78fa31d0abd16a
Anurag Singh Patel <anuragsinghpatel98@gmail.com>
2021-11-02 22:21:04 +0530

    Insertion Sort done

---
+++ Sorting/InsertionSort.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void iSort(int arr[],int n){
+    
+    for(int i=1;i<n;i++){
+        int curr= arr[i];
+        int j=i-1;
+        while(j>=0 && arr[j]>curr){
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=curr;
+    }
+}
+
+int main() {
+    int arr[]={50,20,40,60,10,30};
+	int n=sizeof(arr)/sizeof(arr[0]);
+	iSort(arr,n);
+	for(auto x: arr)
+	    cout<<x<<" ";
+}
