3ab9370a9503f8f15e7f39c107e11bec418d417a
Anurag Singh Patel <anuragsinghpatel98@gmail.com>
2021-11-06 23:13:09 +0530

    Boundry Traversal done

---
+++ Matrix/BoundryTraversal.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int R = 4, C = 4;
+void BoundryTraversal(int mat[R][C]){
+     if(R==1){
+         for(int i=0;i<R;i++)
+             cout<<mat[0][i]<<" ";     
+     }
+     else if{
+
+     }
+}
+int main()
+{	
+	int arr[R][C] = {{1, 2, 3, 4},
+				   {5, 6, 7, 8},
+				   {9, 10, 11, 12},
+				   {13, 14, 15, 16}};
+
+    BoundryTraversal(arr);
+
+	return 0;
+}
