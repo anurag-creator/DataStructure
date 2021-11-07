d37a0dfd0df073cb0010ec96c1444b6758cf44a1
Anurag Singh Patel <anuragsinghpatel98@gmail.com>
2021-11-07 16:02:48 +0530

    GCD done

--- Maths/GCD.cpp
+++ Maths/GCD.cpp
@@ -1,20 +1,31 @@
 #include <iostream>
 #include <limits.h>
 using namespace std;
+//Basic Version
+int gcd1(int a,int b){
+    while(a!=b){
+		if(a>b){
+			a= a-b;
+		}
+		else{
+			b=b-a;
+		}	
+	}
+	return a;
+}
+//Optimize Euclidean Algorithm
 
- int gcd(int a, int b)
-{
+int gcd2(int a, int b){
 	if(b==0)
 		return a;
-
-	return gcd(b, a % b);
+	return gcd2(b, a % b);
 }
 
 int main() {
     
     	int a = 12, b = 15;
-    	
-    	cout<<gcd(a, b);
+    	cout<<gcd1(a, b);
+    	cout<<gcd2(a, b);
     	
     	return 0;
 }
