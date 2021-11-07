b638d58c4410691bd96386ce5408e11324f5e9fa
Anurag Singh Patel <anuragsinghpatel98@gmail.com>
2021-11-07 15:44:20 +0530

    Trailing zero

--- Maths/TrailingZeroInFactorial.cpp
+++ Maths/TrailingZeroInFactorial.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 #include <limits.h>
 using namespace std;
-
- int countTrailingZeros(int n)
-{
+//Naive Approah
+//Complexity ThetaOF(n)
+//OverFlow for higher value
+int countTrailingZero(int n){
+	int fact=1;
+	for(int i=2;i<n;i++)
+		fact=fact*i;
+	int res=0;
+	while(fact%10==0){
+		res++;
+		fact=fact/10;//update fact
+	}
+	return res;
+	
+}
+ int countTrailingZeros(int n){
 	int res = 0;
 
-	for(int i=5; i<=n; i=i*5)
-	{
+	for(int i=5; i<=n; i=i*5){
 		res = res + (n / i);
 	}
 
@@ -15,7 +27,7 @@ using namespace std;
 }
 int main() {
     
-    	int number = 251;
+    	int number = 10;
     	
     	cout<<countTrailingZeros(number);
     	
