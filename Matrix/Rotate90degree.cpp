73b205a4f873662c69139a5cd4009d4a38a85cba
Anurag Singh Patel <anuragsinghpatel98@gmail.com>
2021-11-07 10:01:26 +0530

    Rotation done

---
+++ Matrix/Rotate90degree.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cmath>
+#include <bits/stdc++.h> 
+#include <climits>
+#include <deque>
+using namespace std;
+
+const int n = 4;
+
+void transpose(int mat[n][n]){
+    //Transpose
+
+    for(int i = 0; i < n; i++)
+		for(int j = i + 1; j < n; j++)
+			swap(mat[i][j], mat[j][i]);
+	//Reverse column
+
+	for(int i = 0; i < n; i++){
+	    int low = 0, high = n - 1;
+	    
+	    while(low < high) {
+	        swap(mat[low][i], mat[high][i]);
+	        
+	        low++;
+	        high--;
+	    }
+	}
+}
+
+int main(){	
+	int arr[n][n] = {{1, 2, 3, 4},
+				   {5, 6, 7, 8},
+				   {9, 10, 11, 12},
+				   {13, 14, 15, 16}};
+
+    transpose(arr);
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}	
+
+	return 0;
+}
