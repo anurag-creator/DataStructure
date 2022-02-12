828714ca52ed3bbd597611f1a5e1ca99900998a4
Anurag Singh Patel <anuragsinghpatel98@gmail.com>
2022-02-12 23:58:58 +0530

    Swap Number Method

---
+++ Maths/SwapNumberTech.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+//Mehod 1 WITHOUT using 3rd variable
+/*
+int main(){
+    int x=10,y=5;
+    x=x+y;//x=15
+    y=x-y;//y=10
+    x=x-y;//5
+    cout<<x<<" "<<y<<endl;
+}*/
+//Method 2
+
+int main(){
+    int x=10,y=5;
+    x=x*y;//50
+    y=x/y;//10
+    x=x/y;
+    cout<<x<<" "<<y<<endl;
+}
+
+//method 3
