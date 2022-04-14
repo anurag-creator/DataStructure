5fa05716ddd101252015cce559e09d8ae194f1a5
Anurag Singh Patel <anuragsinghpatel98@gmail.com>
2022-04-15 01:28:17 +0530

    Implement two stack

--- Stack/ImplementTwoStackInArray.cpp
+++ Stack/ImplementTwoStackInArray.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+/*
 struct TwoStacks { 
     int* arr; 
     int cap; 
@@ -65,8 +65,76 @@ struct TwoStacks {
             exit(1); 
         } 
     } 
-}; 
+}; */
   
+  //Efficient way
+
+  #include <bits/stdc++.h>
+using namespace std;
+
+struct TwoStacks { 
+    int* arr; 
+    int cap; 
+    int top1, top2; 
+    
+    TwoStacks(int n)  
+    { 
+        cap = n; 
+        arr = new int[n]; 
+        top1 = -1; 
+        top2 = cap; 
+    } 
+  
+    void push1(int x) 
+    { 
+        if (top1 < top2 - 1) { 
+            top1++; 
+            arr[top1] = x; 
+        } 
+        else { 
+            cout << "Stack Overflow"; 
+            exit(1); 
+        } 
+    } 
+  
+    void push2(int x) 
+    { 
+        if (top1 < top2 - 1) { 
+            top2--; 
+            arr[top2] = x; 
+        } 
+        else { 
+            cout << "Stack Overflow"; 
+            exit(1); 
+        } 
+    } 
+  
+    int pop1() 
+    { 
+        if (top1 >= 0) { 
+            int x = arr[top1]; 
+            top1--; 
+            return x; 
+        } 
+        else { 
+            cout << "Stack UnderFlow"; 
+            exit(1); 
+        } 
+    } 
+  
+    int pop2() 
+    { 
+        if (top2 < cap) { 
+            int x = arr[top2]; 
+            top2++; 
+            return x; 
+        } 
+        else { 
+            cout << "Stack UnderFlow"; 
+            exit(1); 
+        } 
+    } 
+};
 int main() 
 { 
     TwoStacks ts(5); 
