e74760eba50fb781c6796cec4dfc1864cf1b78d2
Anurag Singh Patel <anuragsinghpatel98@gmail.com>
2022-05-05 10:35:58 +0530

    Optimise sol

--- Arry/PossitiveNegativePair.cpp
+++ Arry/PossitiveNegativePair.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
+//Method 1
 void MoveAllNegativePossitive(int *arr,int n){
     for(int i=0;i<n;i++){
         for(int j=i;j<n;j++){
@@ -9,6 +11,27 @@ void MoveAllNegativePossitive(int *arr,int n){
         }
     }
 }
+//Method 2
+//  n log(n)
+void PossitiveNegativePair(int *arr,int n){
+    bool pair=false;
+    //sort the Arry
+    sort(arr,arr+n);
+    for(int i=0;i<n;i++){
+        if(arr[i]<0){
+            if(binary_search(arr,arr+n,-arr[i])){
+                cout<<arr[i]<<" "<<-arr[i]<<" ";
+                pair=true;
+            }
+        }
+        else{
+            break;
+        }
+    }
+    if(pair==false){
+        cout<<"No Pair Found"<<endl;
+    }
+}
 int main(){
     int n;
     cin>>n;
@@ -16,5 +39,6 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    MoveAllNegativePossitive(arr,n);
+    //MoveAllNegativePossitive(arr,n);
+    PossitiveNegativePair(arr,n);
 }
