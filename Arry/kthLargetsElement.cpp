d995d0586843da33b7d4e5373658fee4a59d1536
Anurag Singh Patel <anuragsinghpatel98@gmail.com>
2022-08-21 17:46:17 +0530

    Kth Largest

---
+++ Arry/kthLargetsElement.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> pq;
+        for(int x: nums)
+            pq.push(x);
+        
+        for(int i=0; i<k-1; i++)
+            pq.pop();
+        return pq.top();
+        
+    
+    }
+};
